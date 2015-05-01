c5663308882ff9d368913f143a91239538d35daf
Phill Dellow <philldellow@gmail.com>
2015-05-01 13:07:43 +1200

    created the gameboard from a highjack in jQuery from the home index page

---
+++ MazeSquared/Scripts/ModelView.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    //hijack the html and make a board
+    var square = 1;
+    var linelimit = 65;
+    $('body').empty();
+    $('body').append("<div class = 'gameBoard'></div>");
+    for (i = 1; i < linelimit; i++) {
+        $('.gameBoard').append("<div class ='tile' id=" + '"' + i + '"' + '></div>');
+        if (i % 8 == 0) {
+            $('.gameBoard').append("</br>");
+        }
+    }
+    //Jsons to css style the divs - will have to live here out of nessesity for now
+    $('.gameBoard').css({
+        'height': '66%', 'width': '66%', 'border':'3px solid black', 'display':'block','margin-left':'auto', 'margin-right':'auto', 'text-align':'center', 'padding-top':'50px', 'padding-bottom':'50px'
+    })
+    $('.tile').css({ 'height': '40px', 'width': '40px', 'border': '1px solid black', 'display': 'inline-block', 'margin-left':'7px' })
+    
+  
+});
+
+          
+    
