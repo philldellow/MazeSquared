69f7fe4e9ef40d25b42160fe1819f0dbee9b03b2
philldellow <philldellow@gmail.com>
2015-05-01 22:53:37 +1200

    added the ajax get calls for GetStart GetActiveSquare and GetNextSquare. Also added css classes to change background colors when ajax calls return a change in id'ed cells class

--- MazeSquared/MazeSquared/Scripts/ModelView.js
+++ MazeSquared/MazeSquared/Scripts/ModelView.js
@@ -10,15 +10,17 @@
             $('.gameBoard').append("</br>");
         }
     }
-    //Jsons to css style the divs - will have to live here out of nessesity for now
+    //Jsons to css style the divs - will have to live here out of messysiticy for now
     $('.gameBoard').css({
         'background-color': '#6EBDBF',
         'height': '66%', 'width': '66%', 'border':'3px solid black', 'display':'block','margin-left':'auto', 'margin-right':'auto', 'text-align':'center', 'padding-top':'50px', 'padding-bottom':'50px', 
     })
     $('.tile').css({ 'height': '40px', 'width': '40px', 'border': '1px solid black', 'display': 'inline-block', 'margin-left': '7px', 'background-color': '#A0DAE5' })
-    //Action changing cllass on enter and leave
+    $('startActivated').css({ 'background-color': '#f5fffa' });
+    $('activated').css({ 'background-color': '#f5fffa' });
+    $('nextSquare').css({ 'background-color': '#fa8072' });
+    //Action changing class on enter and leave
     $(".tile").mouseenter(function () {
-
         $(this).css({ 'background-color': '#E2FFFF' });
     });
     $('.tile').mouseleave(function () {
