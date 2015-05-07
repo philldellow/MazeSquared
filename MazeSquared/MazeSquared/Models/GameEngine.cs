2dd7b74564ce80143aa175050cddc1628ebd0363
Phill Dellow <philldellow@gmail.com>
2015-05-07 22:11:35 +1200

    modified the random start so it now only slects an outside square to begin

--- MazeSquared/MazeSquared/Models/GameEngine.cs
+++ MazeSquared/MazeSquared/Models/GameEngine.cs
@@ -24,10 +24,25 @@ namespace MazeSquared.Models
 
         public static int randomStart()
         {
-            int j = rnd.Next(0, 63);
+            //int j = rnd.Next(0, 63);
+            int j = selectRandomStart();
             gameBoard.Remove(j);
             gameBoard.Add(j,"start");
             return weIsUpToHere = j;
+        }
+
+        public static List<int> borderSquares()
+        {
+            List<int> outlineSquares = new List<int>();
+            outlineSquares.InsertRange(outlineSquares.Count, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 17, 25, 33, 41, 49, 16, 24, 32, 40, 48, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65 });          
+            return outlineSquares;
+        }
+
+        public static int selectRandomStart()
+        {
+            int countOfBorderSquares = borderSquares().Count;
+            int r = rnd.Next(0,countOfBorderSquares);
+            return borderSquares()[r];
         }
 
         public static int activeSquare()
